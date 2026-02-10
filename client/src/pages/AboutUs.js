32973414856dae97be8bdbfb69995a7fe8e50ba4
Anvarjon <anvarqosimov153@gmail.com>
2026-02-10 23:43:02 +0500

    site was finished to 100%

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,7 +1,13 @@
-export default function AboutUs() {
+import "../styles/AboutUs.css"
+
+function AboutUs() {
     return (
-        <div>
-            
+        <div className="AboutUs">
+            <div className="aboutUsText">
+                <h1>Bizda chizmachilik va dizayn xizmatlari mavjud, shuning uchun biz mijozlarimizga o'zlarining orzularidagi uylarni yaratishda yordam beramiz. Bizning jamoamiz tajribali va kreativ dizaynerlardan iborat bo'lib, ular mijozlarimizning istaklarini tinglab, ularni amalga oshirish uchun eng yaxshi yechimlarni taklif qilishadi. Biz mijozlarimizga o'zlarining orzularidagi uylarni yaratishda yordam beramiz va ularning hayotini yanada qulay va go'zal qilishga intilamiz.</h1>
+            </div>
         </div>
     )
 }
+
+export default AboutUs
