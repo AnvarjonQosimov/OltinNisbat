36d8cea5d565444236b940ec69460727f0beb769
Anvarjon <anvarqosimov153@gmail.com>
2026-06-01 10:18:32 +0500

    the site was almost finished

--- client/src/components/FullSlider.js
+++ client/src/components/FullSlider.js
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Thumbs } from "swiper/modules";
-import Zoom from "react-medium-image-zoom";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/thumbs";
@@ -10,81 +9,103 @@ import "../styles/FullSlider.css";
 function FullSlider({ media }) {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const mainSwiperRef = useRef(null);
+  const [zoomedImage, setZoomedImage] = useState(null);
   const hasMultiple = media.length > 1;
 
+  const handleImageClick = (url) => {
+    setZoomedImage(url);
+  };
+
+  const closeZoom = () => {
+    setZoomedImage(null);
+  };
+
   return (
-    <div className="fullSliderWrapper">
-      <div className="mainSliderContainer">
-        {hasMultiple && (
-          <button
-            className="customSliderBtn customSliderBtnLeft"
-            onClick={() => mainSwiperRef.current?.swiper.slidePrev()}
+    <>
+      <div className="fullSliderWrapper">
+        <div className="mainSliderContainer">
+          {hasMultiple && (
+            <button
+              className="customSliderBtn customSliderBtnLeft"
+              onClick={() => mainSwiperRef.current?.swiper.slidePrev()}
+            >
+              ‹
+            </button>
+          )}
+
+          <Swiper
+            ref={mainSwiperRef}
+            modules={[FreeMode, Thumbs]}
+            thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+            className="mainSlider"
+            loop={hasMultiple}
           >
-            ‹
-          </button>
-        )}
+            {media.map((file, index) => {
+              const url = `http://localhost:8070/${file}`;
+              const isVideo = file.endsWith(".mp4") || file.endsWith(".mov") || file.endsWith(".avi");
 
-        <Swiper
-          ref={mainSwiperRef}
-          modules={[FreeMode, Thumbs]}
-          thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-          className="mainSlider"
-          loop={hasMultiple}
-        >
-          {media.map((file, index) => {
-            const url = `http://localhost:8070/${file}`;
-            const isVideo = file.endsWith(".mp4") || file.endsWith(".mov") || file.endsWith(".avi");
-
-            return (
-              <SwiperSlide key={index} className="mainSwiperSlide">
-                {isVideo ? (
-                  <video src={url} controls className="fullSliderMedia" />
-                ) : (
-                  <Zoom>
+              return (
+                <SwiperSlide key={index} className="mainSwiperSlide">
+                  {isVideo ? (
+                    <video src={url} controls className="fullSliderMedia" />
+                  ) : (
                     <img
                       src={url}
                       alt={`Full Media ${index}`}
                       className="fullSliderMedia"
+                      onClick={() => handleImageClick(url)}
                     />
-                  </Zoom>
-                )}
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
+                  )}
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+
+          {hasMultiple && (
+            <button
+              className="customSliderBtn customSliderBtnRight"
+              onClick={() => mainSwiperRef.current?.swiper.slideNext()}
+            >
+              ›
+            </button>
+          )}
+        </div>
 
         {hasMultiple && (
-          <button
-            className="customSliderBtn customSliderBtnRight"
-            onClick={() => mainSwiperRef.current?.swiper.slideNext()}
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            modules={[FreeMode, Thumbs]}
+            spaceBetween={10}
+            slidesPerView="auto"
+            freeMode={true}
+            watchSlidesProgress={true}
+            className="thumbsSlider"
           >
-            ›
-          </button>
+            {media.map((file, index) => (
+              <SwiperSlide key={index} className="thumbSwiperSlide">
+                <img
+                  src={`http://localhost:8070/${file}`}
+                  alt={`Thumb ${index}`}
+                  className="thumbImage"
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         )}
       </div>
 
-      {hasMultiple && (
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          modules={[FreeMode, Thumbs]}
-          spaceBetween={10}
-          slidesPerView="auto"
-          freeMode={true}
-          watchSlidesProgress={true}
-          className="thumbsSlider"
-        >
-          {media.map((file, index) => (
-            <SwiperSlide key={index} className="thumbSwiperSlide">
-              <img
-                src={`http://localhost:8070/${file}`}
-                alt={`Thumb ${index}`}
-                className="thumbImage"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
+      {/* Zoom Modal */}
+      {zoomedImage && (
+        <div className="zoomOverlay" onClick={closeZoom}>
+          <img
+            src={zoomedImage}
+            alt="Zoomed"
+            className="zoomedImage"
+            onClick={closeZoom}
+          />
+        </div>
       )}
-    </div>
+    </>
   );
 }
 
